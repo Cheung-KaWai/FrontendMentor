2f1776b525624e38a57f1668564e9bff960fd0a1
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-07-07 22:32:51 +0200

    Add form create columns

---
+++ kanban-task-management-web-app/kanban/src/components/dashboard/AddColumns.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../../styles/dashboard.scss";
+import { Formik, Field, Form, FieldArray } from "formik";
+import { ReactComponent as DeleteInput } from "../../images/input-cross.svg";
+
+export default function AddColumns({ hideForm }) {
+  const addColumns = (values) => {
+    hideForm();
+    //Save columns in redux
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  return (
+    <div className="addColumns">
+      <h2>Platform Launch</h2>
+      <Formik initialValues={{ columns: [""] }} onSubmit={addColumns}>
+        {({ values }) => (
+          <Form>
+            <h3>Board Columns</h3>
+            <FieldArray name="columns">
+              {({ remove, push }) => (
+                <div>
+                  <div className="listInputs">
+                    {values.columns.length > 0 &&
+                      values.columns.map((column, index) => (
+                        <div key={index} className="inputField">
+                          <label htmlFor={`columns.${index}`}>
+                            <Field
+                              name={`columns.${index}`}
+                              className="input"
+                            />
+                          </label>
+                          <DeleteInput onClick={() => remove(index)} />
+                        </div>
+                      ))}
+                  </div>
+                  <button
+                    className="addButton"
+                    onClick={() => push("")}
+                    type="button"
+                  >
+                    Add New Column
+                  </button>
+                  <button className="saveButton">Save Changes</button>
+                </div>
+              )}
+            </FieldArray>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
--- kanban-task-management-web-app/kanban/src/components/dashboard/Empty.jsx
+++ kanban-task-management-web-app/kanban/src/components/dashboard/Empty.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import "../../styles/dashboard.scss";
+import AddColumns from "./AddColumns";
 
 export default function Empty() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <div className="empty">
       <p>This board is empty. Create a new column to get started.</p>
-      <AddColumnButton />
+      <AddColumnButton toggleForm={setShowForm} />
+      {showForm && <AddColumns hideForm={() => setShowForm(false)} />}
+      {showForm && (
+        <div className="darkBackground" onClick={() => setShowForm(false)} />
+      )}
     </div>
   );
 }
 
-const AddColumnButton = () => {
-  return <button>Add New Colmun</button>;
+const AddColumnButton = ({ toggleForm }) => {
+  return (
+    <button onClick={() => toggleForm((prev) => !prev)}>Add New Column</button>
+  );
 };
