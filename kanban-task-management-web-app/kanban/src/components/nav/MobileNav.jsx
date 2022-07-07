f94fa7260d7f36a58b6dc2ac7f794603be2168b2
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-07-07 11:22:59 +0200

    Add UI mobile nav

---
+++ kanban-task-management-web-app/kanban/src/components/nav/MobileNav.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../../styles/nav.scss";
+import { ReactComponent as MobileLogo } from "../../images/logo-mobile.svg";
+import { ReactComponent as ArrowDown } from "../../images/icon-chevron-down.svg";
+import { ReactComponent as Settings } from "../../images/icon-vertical-ellipsis.svg";
+import { ReactComponent as Add } from "../../images/icon-add-task-mobile.svg";
+import { useState } from "react";
+
+export default function MobileNav() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="navContainer">
+      <MobileLogo />
+      {console.log(showMenu)}
+      <nav>
+        <h1>
+          <button onClick={() => setShowMenu((prev) => !prev)}>
+            Platform Launch <ArrowDown />
+          </button>
+        </h1>
+        {showMenu && <Menu />}
+      </nav>
+      <AddButton />
+      <Settings />
+    </div>
+  );
+}
+
+const Menu = () => {
+  return (
+    <ul className="submenu">
+      <li>Platform Launch</li>
+      <li>Marketing Plan</li>
+      <li>Roadmap</li>
+      <li>Create new board</li>
+    </ul>
+  );
+};
+
+const AddButton = () => {
+  return (
+    <button className="addButton">
+      <Add />
+    </button>
+  );
+};
---
+++ kanban-task-management-web-app/kanban/src/pages/Board.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MobileNav from "../components/nav/MobileNav";
+
+export default function Board() {
+  return (
+    <div>
+      <MobileNav />
+    </div>
+  );
+}
