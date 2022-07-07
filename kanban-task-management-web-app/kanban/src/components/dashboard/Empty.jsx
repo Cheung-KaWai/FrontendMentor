edc358a75c639e407b6815a024c4609d7bfc5df5
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-07-07 18:56:13 +0200

    Add empty board UI

--- kanban-task-management-web-app/kanban/src/components/dashboard/Empty.jsx
+++ kanban-task-management-web-app/kanban/src/components/dashboard/Empty.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "../../styles/dashboard.scss";
 
 export default function Empty() {
   return (
-    <div>Empty</div>
-  )
+    <div className="empty">
+      <p>This board is empty. Create a new column to get started.</p>
+      <AddColumnButton />
+    </div>
+  );
 }
+
+const AddColumnButton = () => {
+  return <button>Add New Colmun</button>;
+};
--- kanban-task-management-web-app/kanban/src/pages/Board.jsx
+++ kanban-task-management-web-app/kanban/src/pages/Board.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Empty from "../components/dashboard/Empty";
 import MobileNav from "../components/nav/MobileNav";
 
 export default function Board() {
   return (
     <div>
       <MobileNav />
+      <Empty />
     </div>
   );
 }
